65167a90b4471812bf41dc5a93c8831dd81b20ed
SitaGomes <ansitagomes@gmail.com>
2021-12-08 13:46:09 +0100

    Inicial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DashGo | Home</title>
+      </Head>
+      <h1>DashGo</h1>
+    </>
+  )
+}
