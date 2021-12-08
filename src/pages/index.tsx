d10fe44ce800db2a0ca2b93868085cc001b7ce99
SitaGomes <ansitagomes@gmail.com>
2021-12-08 16:30:44 +0100

    Add: Sign in page creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,45 @@
 import Head from 'next/head'
 
-export default function Home() {
+import {Flex, Button, Stack, FormLabel, FormControl} from "@chakra-ui/react"
+
+import {Input} from "../components/Form/input"
+
+export default function SingIn() {
   return (
     <>
       <Head>
         <title>DashGo | Home</title>
       </Head>
-      <h1>DashGo</h1>
+
+      <Flex
+        w="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+
+        <Flex
+          as="form"
+          w="100%"
+          maxW="360px"
+          bg="gray.800"
+          p="8"
+          borderRadius="8"
+          flexDir="column"
+        >
+
+          <Stack spacing="4">
+
+            <Input name="email" label="E-mail" type="email"/>
+            
+            <Input name="password" label="Senha" type="password"/>
+            
+          </Stack>
+
+          <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        </Flex>
+      </Flex>
+    
     </>
   )
 }
